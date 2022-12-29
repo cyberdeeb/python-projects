07014bea087f166cf14f5a7515d11a80d0a33d3a
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2022-12-28 17:41:58 -0800

    Day 1 of 100 Days of Code

---
+++ band_name_generator/main.py
@@ -0,0 +1,12 @@
+# Greeting for program.
+print("Welcome to the band name generator!")
+
+# Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+# Ask the user for the name of a pet.
+pet = input("What was your pets name?\n")
+
+# Combine the name of their city and pet and show them their band name.
+print(f"{city} {pet}")
+print("")
