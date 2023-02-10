e22b4b55d40ee38bb119dd9befe65f248d5658a4
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2023-02-09 19:02:54 -0800

    Updated project using exception handling

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -7,13 +7,20 @@ data = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 # Get users input and format
-word = input('Enter a word: ').strip().upper()
+while True:
+    try:
+        word = input('Enter a word: ').strip().upper()
 
-# Create letter list
-letters = [letter for letter in word]
+        # Create letter list
+        letters = [letter for letter in word]
 
-# Translate each letter using nato_dict
-translate = [nato_dict[letter] for letter in letters]
+        # Translate each letter using nato_dict
+        translate = [nato_dict[letter] for letter in letters]
+    except KeyError:
+        print('Please only input words.')
+    else:
+        print(translate)
+        break
 
-print(translate)
+
 
