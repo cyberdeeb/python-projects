3f445e36ee1c3c4e4186c5488a557bc93dcbd8f8
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2023-03-11 09:56:24 -0800

    Day 38 of 100 Days of Code
    Initial upload of code

---
+++ fitness_tracker/main.py
@@ -0,0 +1,24 @@
+import requests
+
+
+APP_ID = 'cea22567'
+API_KEY = '4b3c42cc0f93f70c6d9ca455b249deda	'
+fitness_url = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+header = {
+    "x-app-id": APP_ID,
+    'x-app-key': API_KEY,
+    "x-remote-user-id": '0',
+}
+
+workout = input('Tell me which exercises you did: ')
+
+params = {
+    "query": workout,
+    "gender": "female",
+    "weight_kg": 72.5,
+    "height_cm": 167.64,
+    "age": 30
+}
+
+response = requests.post(url=fitness_url, json=params, headers=header)
+print(response.text)
